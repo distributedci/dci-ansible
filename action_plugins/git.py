352e97c03f729efd458349114a4f8ed34039a834
Frederic Lepied <flepied@redhat.com>
2022-11-26 17:52:33 +0100

    action_plugins/git.py: expose the component
    Change-Id: Ib2d4bb9b32e09f3b2cd496d9f9675fc67792e12a

--- action_plugins/git.py
+++ action_plugins/git.py
@@ -1,3 +1,18 @@
+#
+# Copyright (C) 2020-2022 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
@@ -130,4 +145,5 @@ class ActionModule(ActionBase):
         elif cmpt.status_code != 201:
             raise ansible_errors.AnsibleError('error while attaching component %s to job %s: %s' % (cmpt_id, job_id, cmpt.text))  # noqa
 
+        module_return['component'] = cmpt.json()
         return module_return
