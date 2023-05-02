688c5da9ef77896cfedbf2c967a3ff5345fbbf57
Nacho Silla <nsilla@redhat.com>
2023-05-02 14:28:09 +0200

    Added warning messages to dci callback output
    Done when:
    - Add the new 5 test cases to run_tests.sh. STARTED
    
    Change-Id: Ia1ef38fe76f47545b2a62c7dc24d66705af4fd1f

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -21,7 +21,7 @@ function clean_db() {
 # --- Starting unit-tests
 
 function run_unit_tests() {
-    modules='dci_user dci_team dci_topic dci_component dci_feeder dci_product dci_job dci_keys'
+    modules='dci_user dci_team dci_topic dci_component dci_feeder dci_product dci_job dci_keys dci_callback/warning_single_item_ok'
 
     source ./admin.sh
     for module in $modules; do
