ce9de66ce8c2c5efe65a8d2668e492eff1eaabd0
Nacho Silla <nsilla@redhat.com>
2023-05-02 14:28:09 +0200

    Added warning messages to dci callback output
    Change-Id: Ia1ef38fe76f47545b2a62c7dc24d66705af4fd1f

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -34,7 +34,7 @@ function run_unit_tests() {
 # --- Start plugin tests
 function run_plugin_tests() {
     plugins='filter_plugins/version_sort'
-
+    
     for plugin in $plugins; do
         ansible-playbook unit-tests/${plugin}/playbook.yml -v
     done
